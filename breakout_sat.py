ab6ace49fc56c5175ea80f9a552555aa2c7c9b8e
Miles Gould <miles@assyrian.org.uk>
2022-04-25 17:26:10 +0100

    Use Z3 API in SAT version

--- breakout_sat.py
+++ breakout_sat.py
@@ -5,6 +5,8 @@ from math import ceil
 import re
 import sys
 
+from z3 import And, BoolSort, Function, IntSort, Not, Optimize, Or, Solver
+
 LINE = re.compile(r'(\w+)\s*=\s*(\d+);')
 
 
@@ -18,78 +20,77 @@ def read_dzn(filename):
     return vars['people'], vars['rooms'], vars['sessions']
 
 
-def loc(i, j, k):
-    """(l i j k) is true if person i is in room j during session k"""
-    return f"(l {i} {j} {k})"
-
-
-def nott(clause):
-    """Negation of clause"""
-    return f"(not {clause})"
-
-
-def andd(clauses):
-    """Conjunction of clauses"""
-    return "(and " + " ".join(clauses) + ")"
-
-
-def orr(clauses):
-    """Disjunction of clauses"""
-    return "(or " + " ".join(clauses) + ")"
-
-
-def person_in_room(p, r, s, rooms):
+def person_in_room(loc, p, r, s, rooms):
     """Person p is in room r (and only room r) during session s"""
     clauses = [
-        nott(loc(p, s, rr)) for rr in range(r)
+        Not(loc(p, rr, s)) for rr in range(r)
     ] + [
-        loc(p, s, r)
+        loc(p, r, s)
     ] + [
-        nott(loc(p, s, rr)) for rr in range(r+1, rooms)
+        Not(loc(p, rr, s)) for rr in range(r+1, rooms)
     ]
-    return andd(clauses)
+    return And(clauses)
 
 
-def one_room_per_session(people, rooms, sessions):
+def one_room_per_session(opt, loc, people, rooms, sessions):
     """Everyone is in exactly one room per session"""
     for p in range(people):
         for s in range(sessions):
-            print(orr(person_in_room(p, r, s, rooms) for r in range(rooms)))
+            opt.add(Or(
+                *(person_in_room(loc, p, r, s, rooms) for r in range(rooms))))
 
 
-def people_not_in_room(room, session, people, absent):
+def people_not_in_room(loc, room, session, people, absent):
     for absentees in combinations(range(people), absent):
-        yield andd(nott(loc(p, room, session)) for p in absentees)
+        yield And(*(Not(loc(p, room, session)) for p in absentees))
 
 
-def rooms_within_capacity(people, rooms, sessions):
+def rooms_within_capacity(opt, loc, people, rooms, sessions):
     capacity = ceil(people / rooms)
     absent = people - capacity
     for r in range(rooms):
         for s in range(sessions):
-            print(orr(people_not_in_room(r, s, people, absent)))
+            opt.add(Or(*people_not_in_room(loc, r, s, people, absent)))
 
 
-def everyone_meets_everyone(people, rooms, sessions):
+def everyone_meets_everyone(opt, loc, people, rooms, sessions):
     for (p1, p2) in combinations(range(people), 2):
-        print(orr(
-            andd([loc(p1, room, session), loc(p2, room, session)])
+        opt.add(Or(*(
+            And(loc(p1, room, session), loc(p2, room, session))
             for room in range(rooms) for session in range(sessions)
-        ))
+        )))
+
+
+def print_model(model, loc, people, rooms, sessions):
+    for s in range(sessions):
+        locations = []
+        for p in range(people):
+            for r in range(rooms):
+                if model.evaluate(loc(p, r, s)):
+                    locations.append(r)
+        print(locations)
+
+        groups = []
+        for r in range(rooms):
+            group = set()
+            for p in range(people):
+                if model.evaluate(loc(p, r, s)):
+                    group.add(p)
+            groups.append(group)
+        print(groups)
 
 
 def main(dzn_file):
     people, rooms, sessions = read_dzn(dzn_file)
-    # (l i j k) is true if person i is in room j during session k
-    print("(declare-fun l (Int Int Int) Bool)")
-    print("(assert (and")
-    one_room_per_session(people, rooms, sessions)
-    rooms_within_capacity(people, rooms, sessions)
-    everyone_meets_everyone(people, rooms, sessions)
+    # (loc i j k) is true if person i is in room j during session k
+    loc = Function('loc', IntSort(), IntSort(), IntSort(), BoolSort())
+    opt = Solver()
+    one_room_per_session(opt, loc, people, rooms, sessions)
+    rooms_within_capacity(opt, loc, people, rooms, sessions)
+    everyone_meets_everyone(opt, loc, people, rooms, sessions)
     # Break symmetries to improve solver speed?
-    print("))")
-    print("(check-sat)")
-    print("(get-model)")
+    print(opt.check())
+    print_model(opt.model(), loc, people, rooms, sessions)
 
 
 if __name__ == '__main__':
